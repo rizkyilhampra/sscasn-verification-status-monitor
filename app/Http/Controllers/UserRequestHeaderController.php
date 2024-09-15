8c5f6e2adfb853dc9f6a9048270ce89de6d294c9
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-09-15 17:17:56 +0800

    chore: separated concern controller

--- app/Http/Controllers/UserRequestHeaderController.php
+++ app/Http/Controllers/UserRequestHeaderController.php
@@ -99,27 +99,4 @@ class UserRequestHeaderController extends Controller
         return redirect()->route('user-request-headers.index');
     }
 
-    private function parseCurlCommand($curlCommand)
-    {
-        $result = [
-            'x_xsrf_token' => '',
-            'cookies' => [],
-        ];
-
-        preg_match("/-H 'X-XSRF-TOKEN: ([^']+)'/", $curlCommand, $matches);
-        if (isset($matches[1])) {
-            $result['x_xsrf_token'] = $matches[1];
-        }
-
-        preg_match("/-H 'Cookie: ([^']+)'/", $curlCommand, $matches);
-        if (isset($matches[1])) {
-            $cookiePairs = explode('; ', $matches[1]);
-            foreach ($cookiePairs as $pair) {
-                [$name, $value] = explode('=', $pair, 2);
-                $result['cookies'][$name] = $value;
-            }
-        }
-
-        return $result;
-    }
 }
