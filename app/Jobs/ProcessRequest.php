c635b5b90733a47c91625fd3774afce8f01d9cd7
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-09-15 17:17:27 +0800

    chore(request): only accept json

--- app/Jobs/ProcessRequest.php
+++ app/Jobs/ProcessRequest.php
@@ -24,7 +24,7 @@ class ProcessRequest implements ShouldQueue
         try {
             $response = Http::withHeaders([
                 'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0',
-                'Accept' => 'application/json, text/plain, */*',
+                'Accept' => 'application/json',
                 'Accept-Language' => 'en-US,en;q=0.5',
                 'Accept-Encoding' => 'gzip, deflate, br, zstd',
                 'X-XSRF-TOKEN' => $this->userRequestHeader->x_xsrf_token,
