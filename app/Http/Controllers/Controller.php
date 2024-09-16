40297a2f4a8ea124de206b8c2f6e18bb964af66d
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-09-17 06:30:59 +0800

    chore(sanitize): throw validation exception when if doesn't match

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,27 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
 abstract class Controller
 {
-    protected function parseCurlCommand($curlCommand)
+    protected function parseCurlCommand(Request $request)
     {
+        $curlCommand = $request->input('curl_command');
         $result = [
             'x_xsrf_token' => '',
             'cookies' => [],
         ];
 
         preg_match("/-H 'X-XSRF-TOKEN: ([^']+)'/", $curlCommand, $matches);
-        if (isset($matches[1])) {
-            $result['x_xsrf_token'] = $matches[1];
+        if (! isset($matches[1])) {
+            throw ValidationException::withMessages([
+                'curl_command' => 'Curl command tidak mengandung XSRF Token yang valid.',
+            ]);
         }
+        $result['x_xsrf_token'] = $matches[1];
 
         preg_match("/-H 'Cookie: ([^']+)'/", $curlCommand, $matches);
-        if (isset($matches[1])) {
-            $cookiePairs = explode('; ', $matches[1]);
-            foreach ($cookiePairs as $pair) {
-                [$name, $value] = explode('=', $pair, 2);
-                $result['cookies'][$name] = $value;
-            }
+        if (! isset($matches[1])) {
+            throw ValidationException::withMessages([
+                'curl_command' => 'Curl command tidak mengandung cookies yang valid.',
+            ]);
+        }
+        $cookiePairs = explode('; ', $matches[1]);
+        foreach ($cookiePairs as $pair) {
+            [$name, $value] = explode('=', $pair, 2);
+            $result['cookies'][$name] = $value;
         }
 
         return $result;
